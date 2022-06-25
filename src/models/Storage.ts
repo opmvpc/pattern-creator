90a43e03a768f96f563226fbc20ce258a485685b
opmvpc <thibsix@outlook.be>
2022-06-25 23:17:21 +0200

    namespace pour storage

--- src/models/Storage.ts
+++ src/models/Storage.ts
@@ -1,20 +1,36 @@
 export class Storage {
+  public static namespace: string = "pattern-creator";
   public static addBoard(name: string, board: string): void {
-    localStorage.setItem(name, board);
+    const boards = Storage.getBoardsFromStorage();
+    if (boards === null) {
+      localStorage.setItem(this.namespace, JSON.stringify({}));
+    }
+    boards[name] = board;
+    localStorage.setItem(this.namespace, JSON.stringify(boards));
   }
 
   public static getBoard(name: string): boolean[] | null {
-    const cellsJSON = localStorage.getItem(name);
+    const boards = Storage.getBoardsFromStorage();
+    const cellsJSON = boards[name];
     if (cellsJSON) {
       return JSON.parse(cellsJSON);
     }
     return null;
   }
 
+  private static getBoardsFromStorage() {
+    const boards = JSON.parse(localStorage.getItem(this.namespace) || "{}");
+    return boards;
+  }
+
   public static getBoardNames(): string[] {
     const names: string[] = [];
-    for (let i = 0; i < localStorage.length; i++) {
-      names.push(localStorage.key(i) ?? "Unnamed");
+    const boards = Storage.getBoardsFromStorage();
+
+    for (const key in boards) {
+      if (Object.prototype.hasOwnProperty.call(boards, key)) {
+        names.push(key);
+      }
     }
     return names.sort();
   }
